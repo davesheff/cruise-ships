f8a608ba7c8a78458aa5a8de705c925187584bed
David Sheffield <davesheff@live.co.uk>
2018-04-02 15:54:08 +0100

    ship, ports, button and message box added

--- src/Controller.js
+++ src/Controller.js
@@ -1,8 +1,12 @@
 /* eslint-env browser */
 /* globals window */
 (function exportShip() {
-  function Controller() {
+  function Controller(ship) {
+    this.ship = ship;
     this.initialiseSea();
+    document.querySelector('#sailbutton').addEventListener('click', () => {
+      this.setSail();
+    });
   }
 
   Controller.prototype = {
@@ -24,11 +28,57 @@
 
     renderPorts(ports) {
       const portsElement = document.querySelector('#ports');
-      portsElement.style.width = 0;
-      ports.forEach(function(port) {
-      })
+      portsElement.style.width = '0px';
+      ports.forEach((port, index) => {
+        const newPortElement = document.createElement('div');
+        newPortElement.dataset.portIndex = index;
+        newPortElement.className = 'port';
+        portsElement.appendChild(newPortElement);
+        const portsElementWidth = parseInt(portsElement.style.width, 10);
+        portsElement.style.width = `${portsElementWidth + 256}px`;
+      });
     },
 
+    renderShip() {
+      const ship = this.ship;
+      const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+      const portElement = document.querySelector(`[data-port-index='${shipPortIndex}']`);
+      const shipElement = document.querySelector('#ship');
+      shipElement.style.top = `${portElement.offsetTop + 32}px`;
+      shipElement.style.left = `${portElement.offsetLeft - 32}px`;
+    },
+
+    setSail() {
+      const ship = this.ship;
+      const currentPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+      const nextPortIndex = currentPortIndex + 1;
+      const nextPortElement = document.querySelector(`[data-port-index='${nextPortIndex}']`);
+      if (!nextPortElement) {
+        return alert('End of the line!');
+      }
+      this.renderMessage(`Now departing ${ship.currentPort.name}`);
+      const shipElement = document.querySelector('#ship');
+      const sailInterval = setInterval(() => {
+        const shipLeft = parseInt(shipElement.style.left, 10);
+        if (shipLeft === (nextPortElement.offsetLeft - 32)) {
+          ship.dock();
+          clearInterval(sailInterval);
+        }
+        shipElement.style.left = `${shipLeft + 1}px`;
+      }, 20);
+      return true;
+    },
+
+    renderMessage(message) {
+      const messageElement = document.createElement('div');
+      messageElement.id = 'message';
+      messageElement.innerHTML = message;
+      const viewport = document.querySelector('#viewport');
+      viewport.appendChild(messageElement);
+      setTimeout(() => {
+        viewport.removeChild(messageElement);
+      }, 2000);
+    },
   };
 
   if (typeof module !== 'undefined' && module.exports) {
